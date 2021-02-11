138fdd26aee6be71275fce3a64c3e710fdddb27e
bintangyandiii <acutyp9@gmail.com>
2021-02-11 09:46:29 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,18 @@
+<?php
+/*
+bintang yandi putra
+203040037
+bintangyandiii/pw2021_203040037
+pertemuan-2-11 februari 2021
+mempelajari mengenai sintaks PHP
+*/
+?>
+
+<?php
+//standar Output
+//echo, print
+//Print_r
+//var_dump
+
+echo "bintangyandiii"
+?>
