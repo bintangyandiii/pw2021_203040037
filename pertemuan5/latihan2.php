9dadea0f65297d6eb09c435f3e2f95b72b21df37
bintangyandiii <acutyp9@gmail.com>
2021-03-16 21:11:34 +0700

    pertemuan6

---
+++ pertemuan5/latihan2.php
@@ -0,0 +1,4 @@
+<?php
+$nama=["bintang yandi putra","Bhatari yandi pertiwi","chresna yandi pratama","andari yandi permata"];
+var_dump($nama)
+?>
---
+++ pertemuan6/latihan2.php
@@ -0,0 +1,54 @@
+<?php
+// $mahasiswa = [
+// ["Bintang Yandi Putra", "203040037", "acutyp9@gmail.com"
+//     , "Teknik Informatika"],
+// ["M Farhan", "203040120", "mfarhan@gmail.com", "
+//     Teknik Industri"]
+// ];
+
+// Array Associative
+// definisinya sama seperti array numerik, kecuali
+// key-nya adalah string yang kita buat sendiri
+$mahasiswa = [
+    [
+    "nama" => "Bintang Yandi Putra", 
+    "nrp" => "203040037",
+    "email" => "acutyp9@gmail.com",
+    "jurusan" => "Teknik Informatika",
+    "gambar" => "IMG-20191026-WA0103"
+    ],
+    [
+        "nama" => "Boy Hendra", 
+        "nrp" => "203040060",
+        "email" => "boyhendra@gmail.com",
+        "jurusan" => "Teknik Mesin",
+        "gambar" => "dom.jpg"
+    ]
+];
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Daftar Mahasiswa</title>
+</head>
+<body>
+	<h1>Daftar Mahasiswa</h1>
+
+	<?php foreach( $mahasiswa as $mhs ) : ?>
+		<ul>	
+            <li>
+                <img src="img/<?= $mhs["gambar"]; ?>">
+            </li>
+			<li>Nama : <?= $mhs["nama"]; ?></li>
+			<li>NRP : <?= $mhs["nrp"]; ?></li>
+			<li>Email : <?= $mhs["email"]; ?></li>
+			<li>Jurusan  : <?= $mhs["jurusan"]; ?></li>
+		</ul>
+	<?php endforeach; ?>
+
+
+
+
+</body>
+</html>
